1ed62eb9a1faa746e4d48685ac680f7639e3ee5b
Sajjad Akhtar <sajjad.akhtar@funavry.com>
2023-08-31 16:38:31 +0500

    https

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,10 +3,14 @@ import type { AppProps } from "next/app";
 import "bootstrap/dist/css/bootstrap.css";
 import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer } from "react-toastify";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+    <Head>
+      <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" /> 
+    </Head>
       <Component {...pageProps} />
       <ToastContainer />
     </>
